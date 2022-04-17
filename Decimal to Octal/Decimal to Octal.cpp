4958d92a8e5f0f761634fd61af46babf64aad419
Ardevengi <103780963+ardevengi@users.noreply.github.com>
2022-04-17 23:06:18 +0200

    Update Decimal to Octal.cpp

--- Decimal to Octal/Decimal to Octal.cpp
+++ Decimal to Octal/Decimal to Octal.cpp
@@ -4,11 +4,11 @@ using namespace std;
 // Function to convert from Decimal to Octal;
 void dToO(int decimal)
 {
-    int octal[33], i;
-    for (int i = 0; decimal != 0; i++)
+    int octal[33], i=0;
+    for (i ; decimal != 0; i++)
     {
         octal[i] = decimal % 8;
-        decimal / 8;
+        decimal /= 8;
     }
     for (int j = i - 1; j >= 0; j--)
     {
